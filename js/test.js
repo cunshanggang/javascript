0cd24910342efb6d6a30fb54167ba71598eb7b4b
cunshanggang <cunshanggang@163.com>
2016-11-19 09:02:47 +0800

    add something

--- js/test.js
+++ js/test.js
@@ -1,1 +1,28 @@
+/**
+ * Created by Administrator on 2016/11/15.
+ */
+/*------------------ How to create an array ---------------------------*/
+//①
+// var tasks = [];
+// tasks[0] = 'First';
+// tasks[1] = 'Second';
+// tasks[2] = 'Third';
+//
+// alert(tasks[0]);
+// alert(tasks[1]);
+// alert(tasks[2]);
+
+
+//②
+// var tasks = ('1','2','3');
+// var tasks = new Array('1','2','3');
+var tasks = [];
+tasks[0] = 1;
+tasks[1] = 2;
+tasks[2] = 3;
+
+alert(tasks[0]);
+alert(tasks[1]);
+alert(tasks[2]);
+
 
